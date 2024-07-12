a860c63eeb01c4b0aa8c4ba3af2425d363ed0fb8
LPeter-nm <pg.nasmatos@outlook.com>
2024-07-12 20:33:30 -0300

    fix: resolvendo conflitos

--- src/componentes/criar/index.jsx
+++ src/componentes/criar/index.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import * as React from "react";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
@@ -6,25 +6,11 @@ import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import { Link } from "react-router-dom";
 import axios from "axios";
-=======
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import { Link, useNavigate } from 'react-router-dom'; 
-import axios from 'axios';
-import { useState } from 'react';
-
-const [setResponse, sizeResponse] = useState("");
-const navigate = useNavigate();
->>>>>>> 515066e2cfb461a179f89a158ef6ced16e8042a7
 
 export default function SignupCard() {
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-<<<<<<< HEAD
     const email = data.get("email");
     const password = data.get("password");
     const name = data.get("name");
@@ -57,38 +43,7 @@ export default function SignupCard() {
         console.error("Erro:", error);
       });
     navigate("/home");
-=======
-    const email = data.get('email');
-    const password = data.get('password');
-    const name = data.get('name');
-    // Perform signup logic here if needed
-    const url = 'http://localhost:4000/usuario/create';
-  
-    const dataJson =  {
-      "username": name, 
-      "email": email,  
-      "accessName": 'Usuario',
-      "password": password,
-    }
 
-    const config = {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    };
-    
-    axios.post(url, dataJson, config)
-      .then(response => {
-        console.log('Resposta:', response.dataJson);
-        setResponse(response.dataJson);
-        navigate('/home');
-      })
-      .catch(error => {
-        console.error('Erro:', error);
-        setResponse(error);
-      });
-
->>>>>>> 515066e2cfb461a179f89a158ef6ced16e8042a7
   };
 
   
@@ -138,13 +93,9 @@ export default function SignupCard() {
             variant="outlined"
             fullWidth
             margin="normal"
-<<<<<<< HEAD
             InputLabelProps={{ style: { color: "white" } }}
             InputProps={{ style: { color: "white" } }}
-=======
-            InputLabelProps={{ style: { color: 'white' } }}
-            InputProps={{ style: { color: 'white' } }}
->>>>>>> 515066e2cfb461a179f89a158ef6ced16e8042a7
+
           />
           <Button
             type="submit"
@@ -157,26 +108,12 @@ export default function SignupCard() {
           </Button>
         </form>
         <Box sx={{ mt: 2 }}>
-<<<<<<< HEAD
           <Link to="/login" style={{ textDecoration: "none" }}>
             <Button
               variant="outlined"
               fullWidth
               sx={{ color: "white", borderColor: "white" }}
-=======
-          <Link to="/login" style={{ textDecoration: 'none' }}>
-            <Button
-              variant="outlined"
-              fullWidth
-              sx={{
-                color: 'white',
-                borderColor: 'white',
-                background: 'linear-gradient(135deg, #3f51b5 0%, #9c27b0 100%)',
-                '&:hover': {
-                  background: 'linear-gradient(135deg, #3f51b5 0%, #9c27b0 100%)',
-                },
-              }}
->>>>>>> 515066e2cfb461a179f89a158ef6ced16e8042a7
+
             >
               Voltar para Login
             </Button>
